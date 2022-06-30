bacdc514fffb8fd7fe5463f6ffebb7639bf7f9b7
Teemo Tebest <teemo.tebest@gmail.com>
2022-06-30 13:23:50 +0200

    Eslinted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { createRoot } from 'react-dom/client';
+
 import App from './jsx/App.jsx';
 
 const container = document.getElementById('app-root-2022-fdi_flows');
